1a4a456c1b98b028e939cb9a530195dd492b142f
khairul anas <mafiakanan@gmail.com>
2021-08-24 09:46:39 +0700

    add hapi + remove error eslint with static keyword

---
+++ math-api/scr/createServer.test.js
@@ -0,0 +1,26 @@
+const createServer = require('./createServer');
+const MathBasic = require('./MathBasic');
+
+describe('A HTTP Server', () => {
+  describe('when GET /add', () => {
+    it('should respond with a status code of 200 and the payload value is addition result of a and b correctly', async () => {
+      // Arrange
+      const a = 10;
+      const b = 20;
+      const spyAdd = jest.spyOn(MathBasic, 'add');
+      const server = createServer({ mathBasic: MathBasic });
+
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: `/add/${a}/${b}`,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual(30);
+      expect(spyAdd).toBeCalledWith(a, b);
+    });
+  });
+});
