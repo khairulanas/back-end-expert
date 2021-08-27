f07b34537c5fa7a26b0aba865e0396cdf815e0f4
khairul anas <mafiakanan@gmail.com>
2021-08-27 10:04:20 +0700

    userRepository

---
+++ auth-api/src/Domains/users/UserRepository.js
@@ -0,0 +1,12 @@
+/* eslint-disable class-methods-use-this */
+class UserRepository {
+  async addUser(newUser) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyAvailableUsername(username) {
+    throw new Error('USER_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = UserRepository;
