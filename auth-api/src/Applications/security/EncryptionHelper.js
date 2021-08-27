3dd9c3fa5131a121c81fc3626d1de1591cf74b8b
khairul anas <mafiakanan@gmail.com>
2021-08-27 14:47:59 +0700

    encription helper

---
+++ auth-api/src/Applications/security/EncryptionHelper.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+class EncryptionHelper {
+  async encryptPassword(password) {
+    throw new Error('ENCRYPTION_HELPER.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = EncryptionHelper;
