c4a1e86a83b1fa13d7a9a4483ca864f5651d8f22
khairul anas <mafiakanan@gmail.com>
2021-08-27 09:39:27 +0700

    addedUser entities

---
+++ auth-api/src/Domains/users/entities/AddedUser.js
@@ -0,0 +1,25 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+class AddedUser {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const { id, username, fullname } = payload;
+
+    this.id = id;
+    this.username = username;
+    this.fullname = fullname;
+  }
+
+  _verifyPayload({ id, username, fullname }) {
+    if (!id || !username || !fullname) {
+      throw new Error('ADDED_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string' || typeof username !== 'string' || typeof fullname !== 'string') {
+      throw new Error('ADDED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = AddedUser;
