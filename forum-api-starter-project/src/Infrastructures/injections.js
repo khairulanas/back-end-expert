f4335ecf437186721f054390104633504b17301c
khairul anas <mafiakanan@gmail.com>
2021-09-01 06:38:29 +0700

    ini submission 1

---
+++ forum-api-starter-project/src/Infrastructures/injections.js
@@ -0,0 +1,52 @@
+/* istanbul ignore file */
+
+// external agency
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const Jwt = require('@hapi/jwt');
+const pool = require('./database/postgres/pool');
+
+// service (repository, helper, manager, etc)
+const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
+const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
+const BcryptEncryptionHelper = require('./security/BcryptEncryptionHelper');
+const JwtTokenManager = require('./security/JwtTokenManager');
+
+// use case
+const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
+const LoginUserUseCase = require('../Applications/use_case/LoginUserUseCase');
+const RefreshAuthenticationUseCase = require('../Applications/use_case/RefreshAuthenticationUseCase');
+const LogoutUserUseCase = require('../Applications/use_case/LogoutUserUseCase');
+
+const serviceInstanceContainer = {
+  userRepository: new UserRepositoryPostgres(pool, nanoid),
+  authenticationRepository: new AuthenticationRepositoryPostgres(pool),
+  encryptionHelper: new BcryptEncryptionHelper(bcrypt),
+  authenticationTokenManager: new JwtTokenManager(Jwt.token),
+};
+
+const useCaseInstanceContainer = {
+  addUserUseCase: new AddUserUseCase({
+    userRepository: serviceInstanceContainer.userRepository,
+    encryptionHelper: serviceInstanceContainer.encryptionHelper,
+  }),
+  loginUserUseCase: new LoginUserUseCase({
+    authenticationRepository: serviceInstanceContainer.authenticationRepository,
+    authenticationTokenManager: serviceInstanceContainer.authenticationTokenManager,
+    userRepository: serviceInstanceContainer.userRepository,
+    encryptionHelper: serviceInstanceContainer.encryptionHelper,
+  }),
+  refreshAuthenticationUseCase: new RefreshAuthenticationUseCase({
+    authenticationRepository: serviceInstanceContainer.authenticationRepository,
+    authenticationTokenManager: serviceInstanceContainer.authenticationTokenManager,
+  }),
+  logoutUserUseCase: new LogoutUserUseCase({
+    authenticationRepository: serviceInstanceContainer.authenticationRepository,
+  }),
+};
+
+// export all instance
+module.exports = {
+  ...serviceInstanceContainer,
+  ...useCaseInstanceContainer,
+};
