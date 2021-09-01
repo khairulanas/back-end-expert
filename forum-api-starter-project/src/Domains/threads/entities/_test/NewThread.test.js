d2a5b34c12dc3a466c81bb254ec9df9540c6b04c
khairul anas <mafiakanan@gmail.com>
2021-09-01 08:03:06 +0700

    new thread entities

---
+++ forum-api-starter-project/src/Domains/threads/entities/_test/NewThread.test.js
@@ -0,0 +1,39 @@
+const NewThread = require('../NewThread');
+
+describe('a NewThread entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      title: 'abc',
+      body: 123,
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create newThread object correctly', () => {
+    // Arrange
+    const payload = {
+      title: 'abc',
+      body: 'abc body',
+    };
+
+    // Action
+    const { title, body } = new NewThread(payload);
+
+    // Assert
+    expect(title).toEqual(payload.title);
+    expect(body).toEqual(payload.body);
+  });
+});
