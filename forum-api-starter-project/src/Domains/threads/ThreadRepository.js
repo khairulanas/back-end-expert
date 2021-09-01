250f4d77d9c7a3e6efd91a5eeb56df1a7b268a7a
khairul anas <mafiakanan@gmail.com>
2021-09-01 10:10:46 +0700

    thread repositori

---
+++ forum-api-starter-project/src/Domains/threads/ThreadRepository.js
@@ -0,0 +1,11 @@
+class ThreadRepository {
+  async addThread(newThread) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThreadById(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = ThreadRepository;
