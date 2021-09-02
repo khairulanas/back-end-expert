1a1f7444bc0e9e4d1ea7ada25720f888cc16b42d
khairul anas <mafiakanan@gmail.com>
2021-09-02 22:45:50 +0700

    ThreadsTableTestHelper

---
+++ forum-api-starter-project/tests/ThreadsTableTestHelper.js
@@ -0,0 +1,35 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'kana',
+    body = 'hanazawa',
+    date = '2021-08-08T07:59:48.766Z',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5)',
+      values: [id, title, body, date, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async getThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
