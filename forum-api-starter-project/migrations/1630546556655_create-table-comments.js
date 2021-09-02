9bdf1770f2051792007054db9ee009a203654306
khairul anas <mafiakanan@gmail.com>
2021-09-02 08:50:40 +0700

    create table comments

---
+++ forum-api-starter-project/migrations/1630546556655_create-table-comments.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments', 'fk_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
