83c7d9eda9dd00223a10c2db5447a04c19e2f894
khairul anas <mafiakanan@gmail.com>
2021-09-03 13:19:08 +0700

    add comment & reply mappingDB

--- forum-api-starter-project/src/Commons/utils/mapdb.js
+++ forum-api-starter-project/src/Commons/utils/mapdb.js
@@ -13,4 +13,29 @@ const mapDBToDetailThread = ({
   username: owner,
 });
 
-module.exports = { mapDBToDetailThread };
+const mapDBToDetailComment = ({
+  id,
+  owner,
+  date,
+  content,
+  replies,
+}) => ({
+  id,
+  username: owner,
+  date,
+  content,
+  replies,
+});
+const mapDBToDetailReply = ({
+  id,
+  owner,
+  date,
+  content,
+}) => ({
+  id,
+  username: owner,
+  date,
+  content,
+});
+
+module.exports = { mapDBToDetailThread, mapDBToDetailComment, mapDBToDetailReply };
