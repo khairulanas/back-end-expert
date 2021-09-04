012ff471d6ec504f6be8bbb42a2c62b8c5389124
khairul anas <mafiakanan@gmail.com>
2021-09-04 08:18:25 +0700

    injections

--- forum-api-starter-project/src/Infrastructures/injections.js
+++ forum-api-starter-project/src/Infrastructures/injections.js
@@ -9,6 +9,9 @@ const pool = require('./database/postgres/pool');
 // service (repository, helper, manager, etc)
 const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
 const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
+const ThreadRepositoryPostgres = require('./repository/ThreadRepositoryPostgres');
+const CommentRepositoryPostgres = require('./repository/CommentRepositoryPostgres');
+const ReplyRepositoryPostgres = require('./repository/ReplyRepositoryPostgres');
 const BcryptEncryptionHelper = require('./security/BcryptEncryptionHelper');
 const JwtTokenManager = require('./security/JwtTokenManager');
 
@@ -17,10 +20,19 @@ const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
 const LoginUserUseCase = require('../Applications/use_case/LoginUserUseCase');
 const RefreshAuthenticationUseCase = require('../Applications/use_case/RefreshAuthenticationUseCase');
 const LogoutUserUseCase = require('../Applications/use_case/LogoutUserUseCase');
+const AddThreadUseCase = require('../Applications/use_case/AddThreadUseCase');
+const GetDetailThreadUseCase = require('../Applications/use_case/GetDetailThreadUseCase');
+const AddCommentUseCase = require('../Applications/use_case/AddCommentUseCase');
+const DeleteCommentUseCase = require('../Applications/use_case/DeleteCommentUseCase');
+const AddReplyUseCase = require('../Applications/use_case/AddReplyUseCase');
+const DeleteReplyUseCase = require('../Applications/use_case/DeleteReplyUseCase');
 
 const serviceInstanceContainer = {
   userRepository: new UserRepositoryPostgres(pool, nanoid),
   authenticationRepository: new AuthenticationRepositoryPostgres(pool),
+  threadRepository: new ThreadRepositoryPostgres(pool, nanoid),
+  commentRepository: new CommentRepositoryPostgres(pool, nanoid),
+  replyRepository: new ReplyRepositoryPostgres(pool, nanoid),
   encryptionHelper: new BcryptEncryptionHelper(bcrypt),
   authenticationTokenManager: new JwtTokenManager(Jwt.token),
 };
@@ -42,6 +54,24 @@ const useCaseInstanceContainer = {
   }),
   logoutUserUseCase: new LogoutUserUseCase({
     authenticationRepository: serviceInstanceContainer.authenticationRepository,
+  }),
+  addThreadUseCase: new AddThreadUseCase({
+    threadRepository: serviceInstanceContainer.threadRepository,
+  }),
+  getDetailThreadUseCase: new GetDetailThreadUseCase({
+    threadRepository: serviceInstanceContainer.threadRepository,
+  }),
+  addCommentUseCase: new AddCommentUseCase({
+    commentRepository: serviceInstanceContainer.commentRepository,
+  }),
+  deleteCommentUseCase: new DeleteCommentUseCase({
+    commentRepository: serviceInstanceContainer.commentRepository,
+  }),
+  addReplyUseCase: new AddReplyUseCase({
+    replyRepository: serviceInstanceContainer.replyRepository,
+  }),
+  deleteReplyUseCase: new DeleteReplyUseCase({
+    replyRepository: serviceInstanceContainer.replyRepository,
   }),
 };
 
